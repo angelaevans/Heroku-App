778dab8cd55d12c0ac2b46e38e49683923e6d641
Angela Evans <aevans@lelandmanagement.com>
2015-09-21 16:41:14 -0400

    Maybe this time

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', function(request, response) {
 });
 
 app.get('/db', function (request, response) {
-  var connectStr = "pg:postgres://hhlorjpztogkxd:_jYL2Fa1mJSepcyKvJk8_S1WJ2@ec2-46-137-159-123.eu-west-1.compute.amazonaws.com:5432/d6q5sdg1cfttg7"
+  var connectStr = "postgres://hhlorjpztogkxd:_jYL2Fa1mJSepcyKvJk8_S1WJ2@ec2-46-137-159-123.eu-west-1.compute.amazonaws.com:5432/d6q5sdg1cfttg7"
   /*console.log(process.env);
   pg.connect(connectStr, function(err, client, done) {
    client.query('SELECT * FROM caught_users', function(err, result) {
@@ -26,7 +26,7 @@ app.get('/db', function (request, response) {
 		});
 	});*/
   pg.connect(connectStr, function(err, client, done) {
-	  console.log(process.env.HEROKU_POSTGRESQL_PUCE_URL);
+	  console.log(process.env.HEROKU_POSTGRESQL_PINK_URL);
     client.query('SELECT * FROM test_table;', function(err, result) {
       done();
       if (err)
